b8941b12747f48ca09a7eada73b28c85ca04c63d
YKS411 <40791366+YKS411@users.noreply.github.com>
2019-08-25 14:16:21 +0800

    一个简单的电商秒杀程序
    一个简单的电商秒杀程序学习

---
+++ 简单秒杀/SeckilDemo/SeckilDemo/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace SeckilDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var start = DateTime.Now;
+            ThreadBuy();
+            var end = DateTime.Now;
+            Console.WriteLine("------操作完成--------");
+            Console.WriteLine($"执行时间:{end - start}");
+            Console.ReadKey();
+            //Console.WriteLine("Hello World!");
+        }
+
+
+        //秒杀
+        static void ThreadBuy()
+        {
+            System.Threading.Thread.Sleep(2 * 1000);
+           var tt = new ThreadPar();
+
+            #region for循环
+            //for(var i = 0; i < 1000000; i++)
+            //{
+            //    var uid= "用户" + i;
+            //    var x = tt.Buy(uid);
+
+            //    if (x.Code == -1)
+            //    {
+            //        Console.WriteLine(uid + ":" + x.Msg);
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine(uid + ":" + x.Msg + "还剩下" + x.Cnt + "件");
+            //    }
+            //}
+
+            #endregion
+
+            ParallelOptions options = new ParallelOptions()
+            {
+                MaxDegreeOfParallelism = 4   //最大三个线程
+            };
+
+            //Parallel.For(0, 1000000, options, (t, state) => 
+             Parallel.For(0, 1000000,(t, state) =>
+
+            {
+                var uid = "用户" + t;
+                var x = tt.Buy(uid);
+
+                if (x.Code == -1)
+                {
+                    Console.WriteLine(uid + ":" + x.Msg);
+                }
+                else
+                {
+                    Console.WriteLine(uid + ":" + x.Msg + "还剩下" + x.Cnt + "件");
+                }
+
+            });
+
+//            tt.Dispose();   //销毁
+        }
+
+    }
+}
---
+++ 简单秒杀/SeckilDemo/SeckilDemo/ThreadPar.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeckilDemo
+{
+    public class ThreadPar
+    {
+        private static int cnt = 10;
+        private static Queue queryAll = new Queue();
+        private static Queue<string> queryCur = new Queue<string>();
+        //包装同步---线程安全
+        Queue mySyncdQ = Queue.Synchronized(queryAll);
+
+
+        //使用并发队列
+        private static ConcurrentQueue<string> cqueryAll = new ConcurrentQueue<string>();
+        private static ConcurrentQueue<string> cqueryCur = new ConcurrentQueue<string>();
+
+
+        static ThreadPar()
+        {
+            HandleQueue();
+        }
+
+         public RetData Buy(string uid)
+        {
+            #region 同步
+            mySyncdQ.Enqueue(uid);
+            if (mySyncdQ.Count > cnt)
+
+            //queryAll.Enqueue(uid);
+            //if (queryAll.Count > cnt)
+            {
+                return new RetData { Code = -1, Msg = "商品抢光了", Cnt = 0 };
+            }
+            queryCur.Enqueue(uid);
+            return new RetData { Code = 0, Msg = "恭喜已抢到", Cnt = cnt - queryAll.Count };
+            #endregion
+
+            #region 并发
+            //cqueryAll.Enqueue(uid);
+            //if (cqueryAll.Count > cnt)
+            //{
+            //    return new RetData { Code = -1, Msg = "商品抢光了", Cnt = 0 };
+            //}
+            //cqueryCur.Enqueue(uid);
+            //return new RetData { Code = 0, Msg = "恭喜已抢到", Cnt = cnt - queryAll.Count };
+            #endregion
+        }
+
+
+        public class RetData
+        {
+            public int Code { get; set; }
+            public string Msg { get; set; }
+            public int Cnt { get; set; }
+        }
+
+        public static void HandleQueue()
+        {
+            Task.Factory.StartNew(() =>
+            {
+                while (true)
+                    if (queryCur.Count > 0)
+                        HandleOrder();
+            });
+
+
+            //并发
+            //Task.Factory.StartNew(() =>
+            //{
+            //    while (true)
+            //        if (cqueryCur.Count > 0)
+            //            HandleOrder();
+            //});
+        }
+
+
+        public static void HandleOrder()
+        {
+            while (queryCur.Count != 0)
+            {
+                Console.WriteLine("处理用户订单中：" + queryCur.Dequeue());
+            }
+
+            //while (cqueryCur.Count != 0)
+            //{
+            //    string id;
+            //    Console.WriteLine("处理用户订单中：" + cqueryCur.TryDequeue(out id));
+            //}
+        }
+
+
+        public void Dispose()
+        {
+                queryAll.Dequeue();        
+        }
+
+    }
+}
